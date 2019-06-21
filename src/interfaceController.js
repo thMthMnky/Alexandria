693f42b1aaf1afbe4e4054345662dc32bdc0e017
Henry Zorrilla <thmthmnky@gmail.com>
2019-06-20 20:57:24 -0700

    [WIP] Header Render Method Refactor: Adding New Test Logic

--- src/interfaceController.js
+++ src/interfaceController.js
@@ -11,14 +11,13 @@ var SECONDARY_FONT_COLOR = '#0000000';
 var SECONDARY_COLOR = '#808080'; // Gray: (128,128,128)
 var TERNARYARY_COLOR = '#C0C0C0'; // Silver: (192,192,192)
 var SIDEDBAR_TITLE = 'Upload Data 2 Drive';
-var SIDEBAR_HEIGTH = '600px';
-var SIDEBAR_WIDTH = '400px';
+var SIDEBAR_HEIGTH = '600';
+var SIDEBAR_WIDTH = '400';
 var HEADER_TEXT_HORZ_ALIGNMENT = 'center';
 var HEADER_TEXT_VERT_ALIGNMENT = 'middle';
+var HEADER_FONT_SIZE = 14;
 var HEADER_COMPONENT_PRIMARY_COLOR ='#e6b8af';
 var HEADER_COMPONENT_SECONDARY_COLOR ='#e6b8af';
-var HEADER_FONT_SIZE = 14;
-
 /**
  * Adds a custom menu with items to show the sidebar and dialog.
  *
@@ -26,30 +25,15 @@ var HEADER_FONT_SIZE = 14;
  */
 function onOpen() {
   var ui = SpreadsheetApp.getUi();
-  ui
-  .createMenu()
+  ui 
+  .createAddonMenu()
   .addItem('GetDriveData', 'showUpLoadBar')
   .addToUi();
-
-  showUploadBar();
 }
 
 /**
  * Opens a sidebar. The sidebar structure is described in the Sidebar.html
  * project file.
- *
- * For example,
-
- function doGet(request) {
-  return HtmlService.createTemplateFromFile('Page')
-      .evaluate();
-}
-
-function include(filename) {
-  return HtmlService.createHtmlOutputFromFile(filename)
-      .getContent();
-}
-
  */
 function showUpLoadBar() {
   var ui = HtmlService.createTemplateFromFile('uploadBar')
@@ -180,13 +164,13 @@ var mimeTypes = {
   'form': 'application/vnd.google-apps.form',
   'fusiontable': 'application/vnd.google-apps.fusiontable',
   'map': 'application/vnd.google-apps.map',
-  'photo': 'application/vnd.google-apps.photo',
+  'photo': 'application/vnd.google-apps.photo',	
   'presentation': 'application/vnd.google-apps.presentation',
   'script': 'application/vnd.google-apps.script',
   'site': 'application/vnd.google-apps.site',
   'spreadsheet': 'application/vnd.google-apps.spreadsheet',
-  'unknown': 'application/vnd.google-apps.unknown',
-  'video': 'application/vnd.google-apps.video',
+  'unknown': 'application/vnd.google-apps.unknown',	
+  'video': 'application/vnd.google-apps.video',	
   'drive-sdk': 'application/vnd.google-apps.drive-sdk'
 };
 
@@ -194,13 +178,13 @@ var mimeTypes_keys = Object.keys(mimeTypes);
 
 
 /**
- * Creates a column object whose keys are the column header names and corresponding
- * values are their respective column number  Note, by default, we give each column
+ * Creates a column object whose keys are the column header names and corresponding 
+ * values are their respective column number  Note, by default, we give each column  
  * its natural index as its initial value.
  *
  * @param {Object} obj The object from which we derive the column names
  * @param {String} sep The type of separator to be used in setting heading titles. By default, sep="."
- *
+ * 
  */
 function Columns(obj, sep, idx){
   sep = sep ? sep : " ";
@@ -220,7 +204,7 @@ function Columns(obj, sep, idx){
           Object.keys(Columns(el, sep, idx)).forEach(function(sK){
             list[key+sep+sK] = idx++;
           });
-        }
+        } 
       });
     }
   }, obj);
@@ -229,10 +213,10 @@ function Columns(obj, sep, idx){
 
 /**
  * This tests the function, @makeCoumn by providing an object representing a database document model.   
- *
+ *    
  * @test
  */
-function test_Columns(){
+function testColumns(){
   var columns = Columns(gVol, "_", 0);
   Object.keys(columns).forEach(function(col){
     Logger.log([col, columns[col]]);
@@ -246,15 +230,17 @@ function Header(columns, opts){
   this.columns = columns;
   this.index =  Object.keys(this.columns);
   this.visible = true;
+  
   this.ss = null;
-  this.sheet =null;
+  this.sheet =null;  
   this.row = opts && opts.start.row ? opts.start.row : 1;
   this.column = opts && opts.start.column ? opts.start.column : 1;
-  this.width = opts && opts.width ? opts.width : 1;
+  this.width = opts && opts.width ? opts.width : 1; 
+  
   this.updatedBy = "";
   this.updatedDate = Date.now();
   this.updatedNum = "";
-}
+} 
 
 Header.prototype.isEqualTo = function(obj){
   var self = this;
@@ -267,12 +253,12 @@ Header.prototype.isEqualTo = function(obj){
   return isSameHeader;
 };
 
-/* Render new header on current active sheet */
-Header.prototype.render = function(opts){
+/* Render new header on current active sheet */ 
+Header.prototype.render = function(opts){ 
   var self = this;
   self.ss =  opts && opts.ss ? opts.ss : SpreadsheetApp.getActiveSpreadsheet();
-  self.sheet = opts && opts.sheet ? opts.sheet: self.ss.getActiveSheet();
-
+  self.sheet = opts && opts.sheet ? opts.sheet: this.ss.getActiveSheet();
+    
   function emptyStringArrayOfLength(num){
     var arr = [];
     while(arr.length < num) arr.push("");
@@ -280,67 +266,49 @@ Header.prototype.render = function(opts){
   }
 
   var _header_rows = [self.index];
-
+  
   while(_header_rows.length < self.width){
     _header_rows.push(emptyStringArrayOfLength(self.index.length));
   }
-
+  
   _header_rows.reverse();
+  
+  var _header = [
+    self.sheet.getRange(self.row, self.column, _header_rows.length, self.index.length), 
+    _header_rows
+  ];
+  
+  // [TODO] Create a function to handle these background settings
+  _header[0]
+  .setBackground(PRIMARY_COLOR) 
+  .setFontColor(PRIMARY_FONT_COLOR)
+  .setHorizontalAlignment(HEADER_TEXT_HORZ_ALIGNMENT)// 'center'
+  .setVerticalAlignment(HEADER_TEXT_VERT_ALIGNMENT)// 'middle'
+  .setFontSize(HEADER_FONT_SIZE)// 14
+  .setValues(_header[1]);
 
-  var components = [{
-    name: "main",
-    range: (function (){ return self.sheet.getRange(self.row, self.column, self.width, self.index.length); })(),
-    values: _header_rows,
-    background: PRIMARY_COLOR,
-    fColor: PRIMARY_FONT_COLOR,
-    fHorz: HEADER_TEXT_HORZ_ALIGNMENT,
-    fVert: HEADER_TEXT_VERT_ALIGNMENT,
-    fSize: HEADER_FONT_SIZE
-  },
-  {
-    name: "componentFields",
-    range: self.sheet.getRange(self.row + 1, self.column + 1, 3, 1),
-    values: [['By:'], ['Date:'], ['\#\{Entries\}']],
-    background: HEADER_COMPONENT_PRIMARY_COLOR,
-    fColor: PRIMARY_FONT_COLOR,
-    fHorz: HEADER_TEXT_HORZ_ALIGNMENT,
-    fVert: HEADER_TEXT_VERT_ALIGNMENT,
-    fSize: HEADER_FONT_SIZE
-  },
-  {
-    name: "compoenentValues",
-    range: self.sheet.getRange(self.row + 1, self.column + 2, 3, 1),
-    values: [[self.updatedBy], [self.updatedDate], [self.updatedNum]],
-    background: HEADER_COMPONENT_SECONDARY_COLOR,
-    fColor: SECONDARY_FONT_COLOR,
-    fHorz: HEADER_TEXT_HORZ_ALIGNMENT,
-    fVert: HEADER_TEXT_VERT_ALIGNMENT,
-    fSize: HEADER_FONT_SIZE
-  }];
+  var _header_metadata = {};
+  _header_metadata.fields = [self.sheet.getRange(self.row + 1, self.column + 1, 3, 1), [['By:'], ['Date:'], ['\#\{Entries\}']]];
+  _header_metadata.values = [self.sheet.getRange(self.row + 1, self.column + 2, 3, 1), [[self.updatedBy], [self.updatedDate], [self.updatedNum]]];
+  
+  _header_metadata.fields[0]
+  .setBackground(HEADER_COMPONENT_PRIMARY_COLOR)
+  .setFontColor(PRIMARY_FONT_COLOR)
+  .setHorizontalAlignment(HEADER_TEXT_HORZ_ALIGNMENT)// 'center'
+  .setVerticalAlignment(HEADER_TEXT_VERT_ALIGNMENT)// 'middle'
+  .setFontSize(HEADER_FONT_SIZE)// 14
+  .setValues(_header_metadata.fields[1]);
 
-  function _render(type, range, values, background, fColor, fHorz, fVert, fSize){
-    range()
-    .setBackground(background)
-    .setFontColor(fColor)
-    .setHorizontalAlignment(fHorz)// 'center'
-    .setVerticalAlignment(fVert)// 'middle'
-    .setFontSize(fSize)// 14
-    .setValues(values);
-  }
-
-  for( var i = 0; i < components.length ){
-   _render(components[i].name,
-	  components[i].range(),
-	  components[i].values,
-	  components[i].background,
-	  components[i].fColor,
-	  components[i].fHorz,
-	  components[i].fVert,
-	  components[i].fSize );
-  }
+  _header_metadata.values[0]
+  .setBackground(HEADER_COMPONENT_SECONDARY_COLOR)
+  .setFontColor(SECONDARY_FONT_COLOR)
+  .setHorizontalAlignment(HEADER_TEXT_HORZ_ALIGNMENT)// 'center'
+  .setVerticalAlignment(HEADER_TEXT_VERT_ALIGNMENT)// 'middle'
+  .setFontSize(HEADER_FONT_SIZE)// 14
+  .setValues(_header_metadata.values[1]);
 };
 
-/* Shift the header X units to the right */
+/* Shift the header X units to the right */ 
 Header.prototype.shift = function(amount, direction){
   var self = this;
 
@@ -378,23 +346,21 @@ Header.prototype.update = function(){};
  * Test for 'makeHeader'
  */
 function testHeader(){
-
   var dice = {};
-  var tests = {};
+  var tests = {}; 
   var candidates = [];
-  var tests = [];
-  var fails = [];
+  var fails = []; 
 
   function getFuncName() {
     return getFuncName.caller.name;
   }
-
+  
   function getTest(testId){
-    var defaultTestId = [4, 4, 5, 0, 0];
+    var defaultTestIds = [4, 4, 5, 0, 0];
 
     // Test-object model
     var testModel = {
-      vols: null,
+      vol: null,
       opts: {
         ss: null,
         sheet: null,
@@ -416,30 +382,38 @@ function testHeader(){
 
     // Given an arbitrary natuaral number, N, this function will return a 'random' second natuaral number in the interval [0,N) 
     var randNat = function(maxSize) { return Math.floor(maxSize*Math.random()); };
-
+    
     // Common initialization values
     var Opts = [null, "", [], {} ];
 
     // Create a die
-    var dice = [
-      Opts.concat([gVol]),
-      Opts.concat([SpreadsheetApp.getActiveSpreadsheet()]),
-      Opts.concat([SpreadsheetApp.getActiveSpreadsheet().getActiveSheet(), SpreadsheetApp.getActiveSheet()]),
-      Opts.concat([NaN, -6, 0, 3]),
-      Opts.concat([NaN, -2, 0, 7]),
-      Opts.concat([NaN, -2, 0, 7]) 
-    ];
+    dice.vol = Opts.concat([gVol]);
+    try{
+      dice.ss = Opts.concat([SpreadsheetApp.getActiveSpreadsheet()]);
+    }catch(e){
+      fails.push(e);
+      // Logger.log(e);
+    }
 
-    // Given an arbitrary positive integer, N, this function will return and randon integer in the interval [0,N) 
-    var randInt = function(N) { return Math.floor(N*Math.random()); };
+    try{
+      dice.sheet = Opts.concat([dice.ss[4].getActiveSheet(), SpreadsheetApp.getActiveSheet()]);
+    }catch(e){
+      fails.push(e);
+      // Logger.log(e);
+    }
+    dice.row = Opts.concat([NaN, 0, randNat(10), -1*randNat(8)]);
+    dice.col = Opts.concat([NaN, 0, randNat(9), -1*randNat(11)]);
+    dice.width = Opts.concat([NaN, 0, randNat(3), -1*randNat(5)]);
 
-    // Roll the dice
-    testModel.vols = dice[0][randInt(dice[0].length)];
-    testModel.opts.ss = dice[1][randInt(dice[1].length)];
-    testModel.opts.sheet = dice[2][randInt(dice[2].length)];
-    testModel.opts.start.row = dice[3][randInt(dice[3].length)];
-    testModel.opts.start.col = dice[4][randInt(dice[4].length)];
-    testModel.opts.width = dice[5][randInt(dice[5].length)];
+    // Set the index based on 'testId' parameters
+    var isValidTestId = testId.constructor.name != "Array" || testId === null;
+    var Idx = Object.keys(idx);
+    Idx.forEach(function(VAL){
+      Logger.log(VAL);
+      var testIdx = Idx.indexOf(VAL, 0)
+      idx[VAL] = !isValidTestId ? randNat( dice[VAL].length ) : !!tesId[testIdx] && typeof testId[testIdx] == "number" ? testId[testIdx] : defaultTestIds[testIdx];
+      Logger.log(idx[VAL]);
+    });
 
     /** [DEP] Keep until we confirm the corresponding section is confirmed to have worked as intended*/ 
 
@@ -458,81 +432,84 @@ function testHeader(){
     testModel.opts.start.row = dice.row[ idx.row ];
     testModel.opts.start.col = dice.col[ idx.col ];
     testModel.opts.width = dice.width[ idx.width ];
-
+    
     return testModel;
   }
-
+  
   function before(){
-      /**
-       * [TODO] Create a sheet in which to run the tests
-       *
+      /** 
+       * [TODO] Create a sheet in which to run the tests 
+       * 
        * "Before I begin runnning the tests, I will create "
-       * */
+       * */ 
     try{
-      SpreadsheetApp.getActiveSheet().clear();
+      // Getting the Active SpreadSheet
+      SpreadsheetApp.getActiveSpreadsheet();
+
+      // Create a new sheet
+
     }catch(e){
-      Logger.log(e);
+      // Logger.log(e);
     }
   }
 
-  /**
-   * [TODO]
-   *
-   *
+  /** 
+   * [TODO] 
+   * 
+   * 
    * */
   function beforeEach(current_idx){
-    tests[current_idx] = getRandomTest();
-
+    tests[current_idx] = getTest([null, null, null, ]);
     try{
       headings = Columns(tests[current_idx].vols, " ", 0);
       options = tests[current_idx].opts;
       candidates.push(new Header(headings, options));
     }catch(e){
-      fails.push("Failed " + beforeEach + " @ index " + current_idx  + " with error: " + e);
-      Logger.log(e);
+      fails.push("Failed " + beforeEach.name + " @ index " + current_idx  + " with error: " + e);
+      // Logger.log(e)
     }
   }
 
   /**
-   * [TODO] Run the test
-   *
+   * [TODO] Run the test 
+   * 
    * "For the candidate at position @current_idx , "
    * */
-  function testing(current_idx){
+  function testingRender(current_idx){
     var testResults = true;
     try{
      testResults = candidates[current_idx].isEqualTo(candidates[current_idx -1]);
     }catch(e){
-      fails.push("Failed " + testing + " @ index " + current_idx  + " with error: " + e);
-      Logger.log(e);
+      fails.push("Failed " + testingRender.name + " @ index " + current_idx  + " with error: " + e);
+      // Logger.log(e);
     }
     Logger.log(testResults);
     return testResults;
   }
-
+  
   function afterEach(current_idx){
     try{
       candidates[current_idx].render();
     }catch(e){
       fails.push("Failed " + afterEach.name + " @ index " + current_idx  + " with error: " + e);
-      Logger.log(e);
+      // Logger.log(e);
     }
   }
-
+  
   function after(){}
-
+  
   function RunTheTests(){
     var passedAll = true;
     before();
     for(var i = 0; i < 7; i++){
       beforeEach(i);
-      passedAll = passedAll && testing(i);
+      passedAll = passedAll && testingRender(i);
       afterEach(i);
     }
     after();
     return passedAll;
   }
-
+  
   return RunTheTests();
 }
 
@@ -542,27 +519,20 @@ function getAllDriveData(){
   var drive_root = DriveApp.getRootFolder();
   var allFiles = {};
   var allFolders = {};
-  var drive_root;
-  try{
-    drive_root = DriveApp.getRootFolder();
-  }catch(e){
-    Logger.log(e);
-  }
-
+  
   function walk(folder, sep){
-    // Get folder data
+     // Get folder data
     var folderName = folder.getName();
     var files = folder.getFiles();
-
 
     while(files.hasNext()){
       var file = files.next();
       var fileId = file.getId();
       var fileName = file.getName();
       var fileType = file.getMimeType();
-
+      
       if(fileType != mimeTypes.folder){
-        allFiles[fileId] = [folderName + sep + fileName, fileType];
+        allFiles[fileId] = [folderName + sep + fileName, fileType]; 
       } else {
         allFolders[folder.getId()] = folderName;
         walk(file, sep);
@@ -570,11 +540,13 @@ function getAllDriveData(){
     }
   }
   walk(drive_root, ".");
-  return { allFiles, allFolders };
+  return [allFiles, allFolders];
 }
 
 function testGetAllDriveData(){
-  var { allFiles, allFolders }  = getAllDriveData();
+  var data = getAllDriveData();
+  var allFiles = data[0];
+  var allFolders = data[1];
   Logger.log(allFiles);
   Logger.log(allFolders);
 }
